0b65632d7bad58b6a9bbeca108ac090b7d9a7113
Mendih Gezer <61405441+MendihGezer@users.noreply.github.com>
2021-08-23 13:24:28 +0300

    Add files via upload

---
+++ Laskin/Laskin/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Laskin
+{
+    partial class Laskin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LukuYksiTB = new System.Windows.Forms.TextBox();
+            this.LukuKaksiTB = new System.Windows.Forms.TextBox();
+            this.LaskuToimitusCB = new System.Windows.Forms.ComboBox();
+            this.TekstiLB = new System.Windows.Forms.Label();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.LaskeBT = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LukuYksiTB
+            // 
+            this.LukuYksiTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LukuYksiTB.Location = new System.Drawing.Point(213, 165);
+            this.LukuYksiTB.Name = "LukuYksiTB";
+            this.LukuYksiTB.Size = new System.Drawing.Size(100, 31);
+            this.LukuYksiTB.TabIndex = 0;
+            // 
+            // LukuKaksiTB
+            // 
+            this.LukuKaksiTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LukuKaksiTB.Location = new System.Drawing.Point(446, 165);
+            this.LukuKaksiTB.Name = "LukuKaksiTB";
+            this.LukuKaksiTB.Size = new System.Drawing.Size(100, 31);
+            this.LukuKaksiTB.TabIndex = 1;
+            // 
+            // LaskuToimitusCB
+            // 
+            this.LaskuToimitusCB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LaskuToimitusCB.FormattingEnabled = true;
+            this.LaskuToimitusCB.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.LaskuToimitusCB.Location = new System.Drawing.Point(319, 165);
+            this.LaskuToimitusCB.Name = "LaskuToimitusCB";
+            this.LaskuToimitusCB.Size = new System.Drawing.Size(121, 33);
+            this.LaskuToimitusCB.TabIndex = 2;
+            // 
+            // TekstiLB
+            // 
+            this.TekstiLB.AutoSize = true;
+            this.TekstiLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TekstiLB.Location = new System.Drawing.Point(552, 168);
+            this.TekstiLB.Name = "TekstiLB";
+            this.TekstiLB.Size = new System.Drawing.Size(24, 25);
+            this.TekstiLB.TabIndex = 3;
+            this.TekstiLB.Text = "=";
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.Location = new System.Drawing.Point(582, 168);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(26, 25);
+            this.VastausLB.TabIndex = 4;
+            this.VastausLB.Text = "X";
+            this.VastausLB.Visible = false;
+            // 
+            // LaskeBT
+            // 
+            this.LaskeBT.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LaskeBT.Location = new System.Drawing.Point(625, 165);
+            this.LaskeBT.Name = "LaskeBT";
+            this.LaskeBT.Size = new System.Drawing.Size(105, 33);
+            this.LaskeBT.TabIndex = 5;
+            this.LaskeBT.Text = "Laske";
+            this.LaskeBT.UseVisualStyleBackColor = true;
+            this.LaskeBT.Click += new System.EventHandler(this.LaskeBT_Click);
+            // 
+            // Laskin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LaskeBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.TekstiLB);
+            this.Controls.Add(this.LaskuToimitusCB);
+            this.Controls.Add(this.LukuKaksiTB);
+            this.Controls.Add(this.LukuYksiTB);
+            this.Name = "Laskin";
+            this.Text = "Yksinkertainen nelilaskin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LukuYksiTB;
+        private System.Windows.Forms.TextBox LukuKaksiTB;
+        private System.Windows.Forms.ComboBox LaskuToimitusCB;
+        private System.Windows.Forms.Label TekstiLB;
+        private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.Button LaskeBT;
+    }
+}
+
