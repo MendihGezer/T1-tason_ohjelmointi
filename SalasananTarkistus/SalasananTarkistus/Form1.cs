15e0060b1a7866fbc2c6b4e4be49dcfdf867d8fe
Mendih Gezer <mendih.gezer@edu.keuda.fi>
2021-08-26 10:55:08 +0300

    Kesken

---
+++ SalasananTarkistus/SalasananTarkistus/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalasananTarkistus
+{
+    public partial class SalasanaForm : Form
+    {
+        public SalasanaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SalasanaPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+
+
+        }
+
+        private void TarkistaBT_Click(object sender, EventArgs e)
+        {
+            if(KayttajaTB.Text == "Memo" && SalasanaTB.Text == "123")
+            {
+                SalasanaPanel.Visible = false;
+                SalasanaOikeinPanel.Visible = true;
+
+            }
+            else
+            {
+
+                VirheViestiLB.Text = "Käyttäjätunnus tai salasana virheellinen!";
+                VirheViestiLB.Visible = true;
+
+            }
+
+        }
+
+
+    }
+}
