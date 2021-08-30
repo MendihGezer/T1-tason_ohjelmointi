b80a2e2c8e41917467c107d4607d06474938be76
Mendih Gezer <mendih.gezer@edu.keuda.fi>
2021-08-30 14:15:12 +0300

    Valmis

---
+++ Autokululaskuri/Autokululaskuri/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Autokululaskuri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.LyhennysLB = new System.Windows.Forms.Label();
+            this.NesteetLB = new System.Windows.Forms.Label();
+            this.VakuutusLB = new System.Windows.Forms.Label();
+            this.MuutLB = new System.Windows.Forms.Label();
+            this.PolttonesteLB = new System.Windows.Forms.Label();
+            this.LyhennysTB = new System.Windows.Forms.TextBox();
+            this.NesteetTB = new System.Windows.Forms.TextBox();
+            this.VakuutusTB = new System.Windows.Forms.TextBox();
+            this.MuutTB = new System.Windows.Forms.TextBox();
+            this.PolttonesteTB = new System.Windows.Forms.TextBox();
+            this.PesutLB = new System.Windows.Forms.Label();
+            this.HuollotLB = new System.Windows.Forms.Label();
+            this.RenkaatLB = new System.Windows.Forms.Label();
+            this.KilometriLB = new System.Windows.Forms.Label();
+            this.PesutTB = new System.Windows.Forms.TextBox();
+            this.HuollotTB = new System.Windows.Forms.TextBox();
+            this.RenkaatTB = new System.Windows.Forms.TextBox();
+            this.KilometriCB = new System.Windows.Forms.ComboBox();
+            this.TuloksetTB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Kristen ITC", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(212, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auton kustannuslaskuri";
+            // 
+            // LyhennysLB
+            // 
+            this.LyhennysLB.AutoSize = true;
+            this.LyhennysLB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LyhennysLB.Location = new System.Drawing.Point(48, 125);
+            this.LyhennysLB.Name = "LyhennysLB";
+            this.LyhennysLB.Size = new System.Drawing.Size(230, 23);
+            this.LyhennysLB.TabIndex = 1;
+            this.LyhennysLB.Text = "Lainan lyhennys korkoineen:";
+            // 
+            // NesteetLB
+            // 
+            this.NesteetLB.AutoSize = true;
+            this.NesteetLB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NesteetLB.Location = new System.Drawing.Point(48, 169);
+            this.NesteetLB.Name = "NesteetLB";
+            this.NesteetLB.Size = new System.Drawing.Size(195, 23);
+            this.NesteetLB.TabIndex = 2;
+            this.NesteetLB.Text = "Lisättävät nesteet yms:";
+            // 
+            // VakuutusLB
+            // 
+            this.VakuutusLB.AutoSize = true;
+            this.VakuutusLB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VakuutusLB.Location = new System.Drawing.Point(48, 213);
+            this.VakuutusLB.Name = "VakuutusLB";
+            this.VakuutusLB.Size = new System.Drawing.Size(146, 23);
+            this.VakuutusLB.TabIndex = 3;
+            this.VakuutusLB.Text = "Vakuutusmaksut:";
+            // 
+            // MuutLB
+            // 
+            this.MuutLB.AutoSize = true;
+            this.MuutLB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MuutLB.Location = new System.Drawing.Point(48, 257);
+            this.MuutLB.Name = "MuutLB";
+            this.MuutLB.Size = new System.Drawing.Size(106, 23);
+            this.MuutLB.TabIndex = 4;
+            this.MuutLB.Text = "Muut kulut:";
+            // 
+            // PolttonesteLB
+            // 
+            this.PolttonesteLB.AutoSize = true;
+            this.PolttonesteLB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PolttonesteLB.Location = new System.Drawing.Point(48, 301);
+            this.PolttonesteLB.Name = "PolttonesteLB";
+            this.PolttonesteLB.Size = new System.Drawing.Size(109, 23);
+            this.PolttonesteLB.TabIndex = 5;
+            this.PolttonesteLB.Text = "Polttoneste:";
+            // 
+            // LyhennysTB
+            // 
+            this.LyhennysTB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LyhennysTB.Location = new System.Drawing.Point(284, 122);
+            this.LyhennysTB.Name = "LyhennysTB";
+            this.LyhennysTB.Size = new System.Drawing.Size(100, 29);
+            this.LyhennysTB.TabIndex = 6;
+            // 
+            // NesteetTB
+            // 
+            this.NesteetTB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NesteetTB.Location = new System.Drawing.Point(284, 167);
+            this.NesteetTB.Name = "NesteetTB";
+            this.NesteetTB.Size = new System.Drawing.Size(100, 29);
+            this.NesteetTB.TabIndex = 7;
+            // 
+            // VakuutusTB
+            // 
+            this.VakuutusTB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VakuutusTB.Location = new System.Drawing.Point(284, 212);
+            this.VakuutusTB.Name = "VakuutusTB";
+            this.VakuutusTB.Size = new System.Drawing.Size(100, 29);
+            this.VakuutusTB.TabIndex = 8;
+            // 
+            // MuutTB
+            // 
+            this.MuutTB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MuutTB.Location = new System.Drawing.Point(284, 257);
+            this.MuutTB.Name = "MuutTB";
+            this.MuutTB.Size = new System.Drawing.Size(100, 29);
+            this.MuutTB.TabIndex = 9;
+            // 
+            // PolttonesteTB
+            // 
+            this.PolttonesteTB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PolttonesteTB.Location = new System.Drawing.Point(284, 302);
+            this.PolttonesteTB.Name = "PolttonesteTB";
+            this.PolttonesteTB.Size = new System.Drawing.Size(100, 29);
+            this.PolttonesteTB.TabIndex = 10;
+            // 
+            // PesutLB
+            // 
+            this.PesutLB.AutoSize = true;
+            this.PesutLB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PesutLB.Location = new System.Drawing.Point(434, 131);
+            this.PesutLB.Name = "PesutLB";
+            this.PesutLB.Size = new System.Drawing.Size(60, 23);
+            this.PesutLB.TabIndex = 11;
+            this.PesutLB.Text = "Pesut:";
+            // 
+            // HuollotLB
+            // 
+            this.HuollotLB.AutoSize = true;
+            this.HuollotLB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HuollotLB.Location = new System.Drawing.Point(434, 176);
+            this.HuollotLB.Name = "HuollotLB";
+            this.HuollotLB.Size = new System.Drawing.Size(75, 23);
+            this.HuollotLB.TabIndex = 12;
+            this.HuollotLB.Text = "Huollot:";
+            // 
+            // RenkaatLB
+            // 
+            this.RenkaatLB.AutoSize = true;
+            this.RenkaatLB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RenkaatLB.Location = new System.Drawing.Point(434, 221);
+            this.RenkaatLB.Name = "RenkaatLB";
+            this.RenkaatLB.Size = new System.Drawing.Size(81, 23);
+            this.RenkaatLB.TabIndex = 13;
+            this.RenkaatLB.Text = "Renkaat:";
+            // 
+            // KilometriLB
+            // 
+            this.KilometriLB.AutoSize = true;
+            this.KilometriLB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KilometriLB.Location = new System.Drawing.Point(434, 266);
+            this.KilometriLB.Name = "KilometriLB";
+            this.KilometriLB.Size = new System.Drawing.Size(141, 23);
+            this.KilometriLB.TabIndex = 14;
+            this.KilometriLB.Text = "Kilometrit/vuosi:";
+            // 
+            // PesutTB
+            // 
+            this.PesutTB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PesutTB.Location = new System.Drawing.Point(588, 128);
+            this.PesutTB.Name = "PesutTB";
+            this.PesutTB.Size = new System.Drawing.Size(100, 29);
+            this.PesutTB.TabIndex = 15;
+            // 
+            // HuollotTB
+            // 
+            this.HuollotTB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HuollotTB.Location = new System.Drawing.Point(588, 173);
+            this.HuollotTB.Name = "HuollotTB";
+            this.HuollotTB.Size = new System.Drawing.Size(100, 29);
+            this.HuollotTB.TabIndex = 16;
+            // 
+            // RenkaatTB
+            // 
+            this.RenkaatTB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RenkaatTB.Location = new System.Drawing.Point(588, 218);
+            this.RenkaatTB.Name = "RenkaatTB";
+            this.RenkaatTB.Size = new System.Drawing.Size(100, 29);
+            this.RenkaatTB.TabIndex = 17;
+            // 
+            // KilometriCB
+            // 
+            this.KilometriCB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.KilometriCB.FormattingEnabled = true;
+            this.KilometriCB.Items.AddRange(new object[] {
+            "5000",
+            "10000",
+            "15000",
+            "20000",
+            "25000",
+            "30000",
+            "35000",
+            "40000"});
+            this.KilometriCB.Location = new System.Drawing.Point(588, 263);
+            this.KilometriCB.Name = "KilometriCB";
+            this.KilometriCB.Size = new System.Drawing.Size(100, 31);
+            this.KilometriCB.TabIndex = 18;
+            this.KilometriCB.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // TuloksetTB
+            // 
+            this.TuloksetTB.AutoSize = true;
+            this.TuloksetTB.Font = new System.Drawing.Font("Kristen ITC", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TuloksetTB.Location = new System.Drawing.Point(52, 368);
+            this.TuloksetTB.Name = "TuloksetTB";
+            this.TuloksetTB.Size = new System.Drawing.Size(299, 23);
+            this.TuloksetTB.TabIndex = 19;
+            this.TuloksetTB.Text = "Kustannukset kilometriä kohti ovat: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TuloksetTB);
+            this.Controls.Add(this.KilometriCB);
+            this.Controls.Add(this.RenkaatTB);
+            this.Controls.Add(this.HuollotTB);
+            this.Controls.Add(this.PesutTB);
+            this.Controls.Add(this.KilometriLB);
+            this.Controls.Add(this.RenkaatLB);
+            this.Controls.Add(this.HuollotLB);
+            this.Controls.Add(this.PesutLB);
+            this.Controls.Add(this.PolttonesteTB);
+            this.Controls.Add(this.MuutTB);
+            this.Controls.Add(this.VakuutusTB);
+            this.Controls.Add(this.NesteetTB);
+            this.Controls.Add(this.LyhennysTB);
+            this.Controls.Add(this.PolttonesteLB);
+            this.Controls.Add(this.MuutLB);
+            this.Controls.Add(this.VakuutusLB);
+            this.Controls.Add(this.NesteetLB);
+            this.Controls.Add(this.LyhennysLB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kilometrikulutuslaskuri";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LyhennysLB;
+        private System.Windows.Forms.Label NesteetLB;
+        private System.Windows.Forms.Label VakuutusLB;
+        private System.Windows.Forms.Label MuutLB;
+        private System.Windows.Forms.Label PolttonesteLB;
+        private System.Windows.Forms.TextBox LyhennysTB;
+        private System.Windows.Forms.TextBox NesteetTB;
+        private System.Windows.Forms.TextBox VakuutusTB;
+        private System.Windows.Forms.TextBox MuutTB;
+        private System.Windows.Forms.TextBox PolttonesteTB;
+        private System.Windows.Forms.Label PesutLB;
+        private System.Windows.Forms.Label HuollotLB;
+        private System.Windows.Forms.Label RenkaatLB;
+        private System.Windows.Forms.Label KilometriLB;
+        private System.Windows.Forms.TextBox PesutTB;
+        private System.Windows.Forms.TextBox HuollotTB;
+        private System.Windows.Forms.TextBox RenkaatTB;
+        private System.Windows.Forms.ComboBox KilometriCB;
+        private System.Windows.Forms.Label TuloksetTB;
+    }
+}
+
