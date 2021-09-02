6baa02e88d8a8168b310b2b1b68df675e36d789f
Mendih Gezer <mendih.gezer@edu.keuda.fi>
2021-09-02 10:09:52 +0300

    Valmis

--- Celcius/Celcius/Form1.Designer.cs
+++ Celcius/Celcius/Form1.Designer.cs
@@ -30,18 +30,19 @@
         {
             this.AsteetLB = new System.Windows.Forms.Label();
             this.AsteetTB = new System.Windows.Forms.TextBox();
-            this.CelciusRB = new System.Windows.Forms.RadioButton();
-            this.FahrenheitRB = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
             this.MuunnaBT = new System.Windows.Forms.Button();
             this.VastausLB = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CelciusRB = new System.Windows.Forms.RadioButton();
+            this.FahrenheitRB = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // AsteetLB
             // 
             this.AsteetLB.AutoSize = true;
             this.AsteetLB.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AsteetLB.Location = new System.Drawing.Point(29, 51);
+            this.AsteetLB.Location = new System.Drawing.Point(12, 47);
             this.AsteetLB.Name = "AsteetLB";
             this.AsteetLB.Size = new System.Drawing.Size(127, 25);
             this.AsteetLB.TabIndex = 0;
@@ -50,19 +51,54 @@
             // AsteetTB
             // 
             this.AsteetTB.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.AsteetTB.Location = new System.Drawing.Point(162, 51);
+            this.AsteetTB.Location = new System.Drawing.Point(145, 47);
             this.AsteetTB.Name = "AsteetTB";
             this.AsteetTB.Size = new System.Drawing.Size(100, 29);
             this.AsteetTB.TabIndex = 1;
             // 
+            // MuunnaBT
+            // 
+            this.MuunnaBT.BackColor = System.Drawing.Color.White;
+            this.MuunnaBT.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MuunnaBT.ForeColor = System.Drawing.Color.Black;
+            this.MuunnaBT.Location = new System.Drawing.Point(443, 41);
+            this.MuunnaBT.Name = "MuunnaBT";
+            this.MuunnaBT.Size = new System.Drawing.Size(114, 45);
+            this.MuunnaBT.TabIndex = 5;
+            this.MuunnaBT.Text = "Muunna";
+            this.MuunnaBT.UseVisualStyleBackColor = false;
+            this.MuunnaBT.Click += new System.EventHandler(this.MuunnaBT_Click);
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.Location = new System.Drawing.Point(12, 109);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(84, 25);
+            this.VastausLB.TabIndex = 6;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FahrenheitRB);
+            this.groupBox1.Controls.Add(this.CelciusRB);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(261, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(161, 92);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Miten muunnat?";
+            // 
             // CelciusRB
             // 
             this.CelciusRB.AutoSize = true;
-            this.CelciusRB.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CelciusRB.Location = new System.Drawing.Point(289, 52);
+            this.CelciusRB.Location = new System.Drawing.Point(6, 28);
             this.CelciusRB.Name = "CelciusRB";
-            this.CelciusRB.Size = new System.Drawing.Size(95, 29);
-            this.CelciusRB.TabIndex = 2;
+            this.CelciusRB.Size = new System.Drawing.Size(81, 25);
+            this.CelciusRB.TabIndex = 0;
             this.CelciusRB.TabStop = true;
             this.CelciusRB.Text = "Celcius";
             this.CelciusRB.UseVisualStyleBackColor = true;
@@ -70,61 +106,29 @@
             // FahrenheitRB
             // 
             this.FahrenheitRB.AutoSize = true;
-            this.FahrenheitRB.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FahrenheitRB.Location = new System.Drawing.Point(289, 85);
+            this.FahrenheitRB.Location = new System.Drawing.Point(6, 54);
             this.FahrenheitRB.Name = "FahrenheitRB";
-            this.FahrenheitRB.Size = new System.Drawing.Size(128, 29);
-            this.FahrenheitRB.TabIndex = 3;
+            this.FahrenheitRB.Size = new System.Drawing.Size(110, 25);
+            this.FahrenheitRB.TabIndex = 1;
             this.FahrenheitRB.TabStop = true;
             this.FahrenheitRB.Text = "Fahrenheit";
             this.FahrenheitRB.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(284, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 25);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Miten muunnat";
-            // 
-            // MuunnaBT
-            // 
-            this.MuunnaBT.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MuunnaBT.Location = new System.Drawing.Point(476, 44);
-            this.MuunnaBT.Name = "MuunnaBT";
-            this.MuunnaBT.Size = new System.Drawing.Size(114, 45);
-            this.MuunnaBT.TabIndex = 5;
-            this.MuunnaBT.Text = "Muunna";
-            this.MuunnaBT.UseVisualStyleBackColor = true;
-            // 
-            // VastausLB
-            // 
-            this.VastausLB.AutoSize = true;
-            this.VastausLB.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.VastausLB.Location = new System.Drawing.Point(29, 127);
-            this.VastausLB.Name = "VastausLB";
-            this.VastausLB.Size = new System.Drawing.Size(84, 25);
-            this.VastausLB.TabIndex = 6;
-            this.VastausLB.Text = "Vastaus";
-            this.VastausLB.Visible = false;
             // 
             // Lomake
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(597, 168);
+            this.ClientSize = new System.Drawing.Size(570, 143);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.VastausLB);
             this.Controls.Add(this.MuunnaBT);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.FahrenheitRB);
-            this.Controls.Add(this.CelciusRB);
             this.Controls.Add(this.AsteetTB);
             this.Controls.Add(this.AsteetLB);
             this.Name = "Lomake";
             this.Text = "Celcius to Fahrenheit";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,11 +138,11 @@
 
         private System.Windows.Forms.Label AsteetLB;
         private System.Windows.Forms.TextBox AsteetTB;
-        private System.Windows.Forms.RadioButton CelciusRB;
-        private System.Windows.Forms.RadioButton FahrenheitRB;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button MuunnaBT;
         private System.Windows.Forms.Label VastausLB;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton FahrenheitRB;
+        private System.Windows.Forms.RadioButton CelciusRB;
     }
 }
 
--- Celcius/Celcius/Form1.cs
+++ Celcius/Celcius/Form1.cs
@@ -16,5 +16,30 @@ namespace Celcius
         {
             InitializeComponent();
         }
+
+        private void MuunnaBT_Click(object sender, EventArgs e)
+        {
+            double vastaus;
+            double asteet = Convert.ToDouble(AsteetTB.Text);
+            if (CelciusRB.Checked)
+            {
+
+                vastaus = asteet * 1.8 + 32;
+                VastausLB.Text = asteet + " Fahrenheitia on " + vastaus + " Celcius astetta";
+                VastausLB.Visible = true;
+            }
+            else if(FahrenheitRB.Checked)
+            {
+                vastaus = (asteet - 32) / 1.8;
+                VastausLB.Text = asteet + " Celciusta on " + vastaus + " Fahrenheit astetta";
+                VastausLB.Visible = true;
+            }
+            else
+            {
+                VastausLB.Text = "Et valinnut muunnettavaa astetta tai oikeaa lukua";
+                VastausLB.Visible = true;
+            }
+
+        }
     }
 }
