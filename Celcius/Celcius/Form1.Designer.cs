e8f5e99810bde857a0202585d3cb20cb54c51b64
Mendih Gezer <mendih.gezer@edu.keuda.fi>
2021-09-02 09:45:58 +0300

    Kesken

---
+++ Celcius/Celcius/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Celcius
+{
+    partial class Lomake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AsteetLB = new System.Windows.Forms.Label();
+            this.AsteetTB = new System.Windows.Forms.TextBox();
+            this.CelciusRB = new System.Windows.Forms.RadioButton();
+            this.FahrenheitRB = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MuunnaBT = new System.Windows.Forms.Button();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AsteetLB
+            // 
+            this.AsteetLB.AutoSize = true;
+            this.AsteetLB.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AsteetLB.Location = new System.Drawing.Point(29, 51);
+            this.AsteetLB.Name = "AsteetLB";
+            this.AsteetLB.Size = new System.Drawing.Size(127, 25);
+            this.AsteetLB.TabIndex = 0;
+            this.AsteetLB.Text = "Anna asteet:";
+            // 
+            // AsteetTB
+            // 
+            this.AsteetTB.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AsteetTB.Location = new System.Drawing.Point(162, 51);
+            this.AsteetTB.Name = "AsteetTB";
+            this.AsteetTB.Size = new System.Drawing.Size(100, 29);
+            this.AsteetTB.TabIndex = 1;
+            // 
+            // CelciusRB
+            // 
+            this.CelciusRB.AutoSize = true;
+            this.CelciusRB.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CelciusRB.Location = new System.Drawing.Point(289, 52);
+            this.CelciusRB.Name = "CelciusRB";
+            this.CelciusRB.Size = new System.Drawing.Size(95, 29);
+            this.CelciusRB.TabIndex = 2;
+            this.CelciusRB.TabStop = true;
+            this.CelciusRB.Text = "Celcius";
+            this.CelciusRB.UseVisualStyleBackColor = true;
+            // 
+            // FahrenheitRB
+            // 
+            this.FahrenheitRB.AutoSize = true;
+            this.FahrenheitRB.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FahrenheitRB.Location = new System.Drawing.Point(289, 85);
+            this.FahrenheitRB.Name = "FahrenheitRB";
+            this.FahrenheitRB.Size = new System.Drawing.Size(128, 29);
+            this.FahrenheitRB.TabIndex = 3;
+            this.FahrenheitRB.TabStop = true;
+            this.FahrenheitRB.Text = "Fahrenheit";
+            this.FahrenheitRB.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(284, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Miten muunnat";
+            // 
+            // MuunnaBT
+            // 
+            this.MuunnaBT.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MuunnaBT.Location = new System.Drawing.Point(476, 44);
+            this.MuunnaBT.Name = "MuunnaBT";
+            this.MuunnaBT.Size = new System.Drawing.Size(114, 45);
+            this.MuunnaBT.TabIndex = 5;
+            this.MuunnaBT.Text = "Muunna";
+            this.MuunnaBT.UseVisualStyleBackColor = true;
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VastausLB.Location = new System.Drawing.Point(29, 127);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(84, 25);
+            this.VastausLB.TabIndex = 6;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // Lomake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(597, 168);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.MuunnaBT);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FahrenheitRB);
+            this.Controls.Add(this.CelciusRB);
+            this.Controls.Add(this.AsteetTB);
+            this.Controls.Add(this.AsteetLB);
+            this.Name = "Lomake";
+            this.Text = "Celcius to Fahrenheit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AsteetLB;
+        private System.Windows.Forms.TextBox AsteetTB;
+        private System.Windows.Forms.RadioButton CelciusRB;
+        private System.Windows.Forms.RadioButton FahrenheitRB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button MuunnaBT;
+        private System.Windows.Forms.Label VastausLB;
+    }
+}
+
